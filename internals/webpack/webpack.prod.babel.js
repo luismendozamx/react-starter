3a0819fb82249b831508358aac4a33be13f03194
Luis Mendoza <luis@mendoza.io>
2016-06-03 18:45:20 -0500

    Fix sass in production

--- internals/webpack/webpack.prod.babel.js
+++ internals/webpack/webpack.prod.babel.js
@@ -21,8 +21,7 @@ module.exports = require('./webpack.base.babel')({
   // of the CSS being in the JS and injected as a style tag
   cssLoaders: ExtractTextPlugin.extract(
     'style-loader',
-    'css-loader?modules&importLoaders=1!',
-    'sass-loader'
+    'css-loader?modules&importLoaders=1!sass-loader'
   ),
 
   plugins: [
